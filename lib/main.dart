46adeff2942045eec8474388c15e58530db5bd22
mymysu <sukanyaboonprasoet629@gmail.com>
2022-03-17 21:13:46 +0700

    add more

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -39,6 +40,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print(userlogin);
+    final Future<FirebaseApp> firebase = Firebase.initializeApp();
+
     return MultiProvider(
         providers: [
           ChangeNotifierProvider<UserProvider>(

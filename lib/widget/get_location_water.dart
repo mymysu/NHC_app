f7d1da533b51f3bc981d307ac234cc7b0b4c214f
mymysu <sukanyaboonprasoet629@gmail.com>
2022-05-12 22:13:05 +0700

    แกื

--- lib/widget/get_location_water.dart
+++ lib/widget/get_location_water.dart
@@ -26,23 +26,14 @@ Future<Position> getGeoLocationPosition() async {
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
       return Future.error('Location permissions are denied');
     }
   }
 
   if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
     return Future.error(
         'Location permissions are permanently denied, we cannot request permissions.');
   }
-
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
   return await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high);
 }
@@ -111,11 +102,6 @@ Future<Water> getAddressFromLatLong(
 
       final int index3 = waterProvider.water.listNameSubdistrict!
           .indexWhere(((value) => value.nameSubdistrict == nameSubdistrict));
-      // print(nameSubdistrict);
-      // print(index3);
-      // print("index1 $index1");
-      // print("index2 $index2");
-      // print("index3 $index3");
 
       waterProvider.water.geographyId =
           waterProvider.water.listNameProvince![index1].geographiesId;
